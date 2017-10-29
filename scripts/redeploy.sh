f1b031047aa8f6db06f840e9e91fb27ba9110762
Clinton Morrison <contact@clintonmorrison.com>
2017-10-29 23:12:30 +0000

    add redeloy script, update nginx config

---
+++ scripts/redeploy.sh
@@ -0,0 +1,8 @@
+
+source venv/bin/activate
+git pull
+git checkout python3
+pip install -r requirements.txt
+python clear_page_cache.py
+python build_page_cache.py
+echo "YOU MUST RESTART WSGI SERVER FOR THESE CHANGES TO TAKE EFFECT."
