854bec8547fea7febe37f1b893398a75c6d92914
Clinton Morrison <contact@clintonmorrison.com>
2017-12-22 16:40:46 -0400

    new, more minimum index page

--- pages/controllers/index.py
+++ pages/controllers/index.py
@@ -2,24 +2,4 @@ from core import database as database
 from pprint import pprint
 
 def get_page_data(path, get, post, variables):
-  data = {'title': 'Clinton Morrison'}
-
-  projects_table = database.Table(u'project')
-  projects = projects_table.filter(
-    conditions = [('hidden', '=', 0)],
-    orderBy = 'date_published',
-    order = 'DESC',
-    limit = 3
-  )
-  data['projects'] = projects
-
-
-  articles_table = database.Table('article')
-  articles = articles_table.filter(
-    orderBy = 'date_published',
-    order = 'DESC',
-    limit = 3
-  )
-  data['articles'] = articles
-
-  return data
+  return {'title': 'Clinton Morrison'}
