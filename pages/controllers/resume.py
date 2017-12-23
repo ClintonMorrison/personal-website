1dc5e8e31cac6e5a03bdc9f36f7aa8a6b4490b64
Clinton Morrison <contact@clintonmorrison.com>
2017-12-23 13:14:19 -0400

    fix some broken pages

--- pages/controllers/resume.py
+++ pages/controllers/resume.py
@@ -1,8 +1,7 @@
-from pprint import pprint
 import yaml
 
 def get_page_data(path, get, post, variables):
-  resume_data = yaml.load(open("static/resume.yaml", "r"))
+  resume_data = yaml.load(open("static/resume.yml", "r"))
   for job in resume_data['work']:
     job['tasks_html'] = "<ul><li>%s</li></ul>" % "</li><li>".join(job['tasks'])
 
