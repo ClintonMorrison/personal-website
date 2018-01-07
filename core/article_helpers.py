9f86d235559519fef9f3b507cdff1f73b7ad9a06
Clinton Morrison <contact@clintonmorrison.com>
2018-01-07 12:50:10 -0500

    add setting for hiding articles

--- core/article_helpers.py
+++ core/article_helpers.py
@@ -3,12 +3,15 @@ from core.functions import format_date_field
 
 def get_all_articles():
   articles = yaml.load(open('static/articles.yml', 'r'))
+  filtered_articles = []
 
   for article in articles:
     article['body'] = get_article_body(article.get('name'))
     format_date_field(article, 'date_published')
+    if article.get('visible'):
+      filtered_articles.append(article)
 
-  return articles
+  return filtered_articles
 
 def get_article_body(name):
   return open('static/blog/{}/article.txt'.format(name), 'r').read()
