50fe8cbf2e521c4d69b3267ec7bbc521e81887dd
Clinton Morrison <contact@clintonmorrison.com>
2019-06-15 16:06:32 -0400

    work on article, make hidden

--- core/markdown.py
+++ core/markdown.py
@@ -119,8 +119,8 @@ class MarkdownParser:
         rules_2 = {
             r"([^\n^<^>]+)\n": self._render_paragraph,
             r"([^\s]*?)\[(.*?)\]": self._render_link,
-            r"\*([^\n]*?)\*": self._render_italics,
-            r"\*\*([^\n]*?)\*\*": self._render_bold
+            r"\*\*([^\n]*?)\*\*": self._render_bold,
+            r"\*([^\n]*?)\*": self._render_italics
         }
 
         rules_3 = {
