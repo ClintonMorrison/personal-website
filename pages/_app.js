1d4f8a721abe43e4162440cc99fa5946aadc0254
Clinton Morrison <clintonmorrison2@gmail.com>
2021-09-10 23:54:56 -0400

    setup next app and linting

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.func.isRequired,
+  pageProps: PropTypes.shape({}).isRequired,
+}
+
+export default MyApp
