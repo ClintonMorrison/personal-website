dd15548a3575385a8ddb9c6fd9f7dabb66209b1f
Clinton Morrison <clintonmorrison2@gmail.com>
2025-01-11 21:19:33 -0500

    fix typos

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ echo "Building version: $VERSION"
 docker stop personal-website
 docker rm personal-website
 
-docker build -t "clintonmorrison/projects:personal-website-$VERSION" . &&
-  docker push "clintonmorrison/projects:personal-website-$VERSION" &&
-  echo "Running container..." &&
-  bash run_container.sh &&
-  docker exec -it personal-website bash
+docker build --platform linux/amd64 -t "clintonmorrison/projects:personal-website-$VERSION" . &&
+  docker push "clintonmorrison/projects:personal-website-$VERSION" 
+
+# echo "Running container..." &&
+# bash run_container.sh &&
+# docker exec -it personal-website bash
